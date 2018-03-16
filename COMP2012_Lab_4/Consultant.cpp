3c8a7920592b13416fe58baa80611a4fe31a9f10
Elliot Ng <chihimng.io@gmail.com>
2018-03-16 16:14:38 +0800

    lab 4: task 2

--- COMP2012_Lab_4/Consultant.cpp
+++ COMP2012_Lab_4/Consultant.cpp
@@ -4,6 +4,7 @@
  */
 
 #include "Consultant.h"
+#include <cmath>
 
 Consultant::Consultant(int cid, string cname, double base): Employee(cid, cname), baseSalary(base) {
 }
@@ -17,8 +18,43 @@ void Consultant::setBaseSalary(double base) {
 }
 
 double Consultant::netSalary(double salary) const {
-	return 0; //Dummy statement, you can delete or modify it after you complete this function.
+	double taxable = salary;
+	double tax = 0;
+	if (taxable < 40000) {
+		return salary; // no tax
+	} else {
+		tax += fmin(40000, taxable) * 0.02;
+		taxable -= 40000;
+	}
+	if (taxable >= 0) {
+		tax += fmin(40000, taxable) * 0.07;
+		taxable -= 40000;
+	}
+	if (taxable >= 0) {
+		tax += fmin(40000, taxable) * 0.12;
+		taxable -= 40000;
+	}
+	if (taxable >= 0) {
+		tax += taxable * 0.17;
+	}
+	return salary - tax;
 }
 
 void Consultant::check(Item item) const {
+	switch(item) {
+	case Item::id:
+		cout << "ID: " << this->getID() << endl;
+		break;
+	case Item::name:
+		cout << "Name: " << this->getName() << endl;
+		break;
+	case Item::base_salary:
+		cout << "Base Salary: " << this->getBaseSalary() << endl;
+		break;
+	case Item::net_salary:
+		cout << "Net Salary: " << this->netSalary(this->baseSalary) << endl;
+		break;
+	default:
+		cout << "Invalid item." << endl;
+	}
 }
